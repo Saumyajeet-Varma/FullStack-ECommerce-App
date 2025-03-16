b4554ffce48095bad1fbb59eff1d983e5e9041a2
Saumyajeet-Varma <saumyajeetv@gmail.com>
2025-03-16 19:35:00 +0530

    checkout button added in cart page

--- frontend/src/pages/Cart.jsx
+++ frontend/src/pages/Cart.jsx
@@ -38,6 +38,20 @@ const Cart = () => {
         }
     }
 
+    const handleCheckout = () => {
+
+        try {
+            const myCart = []
+            setCart(myCart)
+            localStorage.setItem("cart", JSON.stringify(myCart))
+            toast.success("Checked out sucessfully");
+        }
+        catch (error) {
+            console.log(chalk.red(error))
+            toast.error("Failed to checkout")
+        }
+    }
+
     return (
         <Layout>
             <div className="px-20 py-10">
@@ -64,6 +78,19 @@ const Cart = () => {
                                     <>
                                         <h6 className="text-red-600">You need to <a href="/login" className="font-bold">login</a></h6>
                                     </>
+                                )}
+                                {(auth?.user && cart.length) ? (
+                                    <div className="mt-10">
+                                        <button
+                                            onClick={() => handleCheckout()}
+                                            className="rounded-md bg-gray-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-gray-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-gray-600"
+                                        >
+                                            Checkout
+
+                                        </button>
+                                    </div>
+                                ) : (
+                                    <></>
                                 )}
                             </div>
                         </div>
